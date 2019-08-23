ea6325a05fbc3a3e6595ea89c42a0ffb298c4caa
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2019-08-24 01:07:00 +0900

    New upstream release

--- echo-sd.rb
+++ echo-sd.rb
@@ -2,7 +2,7 @@ class EchoSd < Formula
   desc "Echo 'sudden death' message"
   homepage "https://fumiyas.github.io/2013/12/25/echo-sd.sh-advent-calendar.html"
   url "https://raw.githubusercontent.com/fumiyas/home-commands/master/echo-sd"
-  sha256 '11cc142269807da459721e644566f01eece9d0f948aef5968647b4e9acf99b2d'
+  sha256 '6940aa434e50bc41dd8c4499fd0cd333e53b1c8f347969e808e2769db8f47e69'
 
   def install
     bin.install "echo-sd"
