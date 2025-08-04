9bfa8f25a5dc2730b981fdea3ac91e5de42d09b2
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2025-08-04 13:04:01 +0900

    feat: Update to echo-sd 0.0.202508041303

--- echo-sd.rb
+++ echo-sd.rb
@@ -1,9 +1,9 @@
 class EchoSd < Formula
-  version '0.0.202207122231'
+  version '0.0.202508041303'
   desc "Echo 'sudden death' message"
   homepage "https://fumiyas.github.io/2013/12/25/echo-sd.sh-advent-calendar.html"
   url "https://raw.githubusercontent.com/fumiyas/home-commands/master/echo-sd"
-  sha256 '3b0a92f19a1af470fe42e30e8c6f262fbb23960ed6b59acd0c8c2afda4097939'
+  sha256 '9fb4cf2c59565bc094b0789ce178ad1b477082f7539f1639ff8db62aeee9a976'
 
   def install
     bin.install "echo-sd"
